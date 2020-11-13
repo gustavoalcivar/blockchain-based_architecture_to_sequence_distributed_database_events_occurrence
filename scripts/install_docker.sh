28b2db3facc458e006a4b5694625423ceddbe610
Gustavo <gustavoalfonso05@hotmail.com>
2020-11-13 00:33:32 -0500

    first commit

---
+++ scripts/install_docker.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+echo Install Docker...
+sudo apt-get update
+sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt-get update
+apt-cache policy docker-ce
+sudo apt-get -y install docker-ce
+echo Install Docker Compose...
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
